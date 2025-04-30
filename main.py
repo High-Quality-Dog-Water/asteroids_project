8c0b9c3fd106e6304e63fbe5e469a32d1cc2f252
High-Quality-Dog-Water <brettd.work@gmail.com>
2025-04-30 17:30:16 -0600

    B: fixed screen fill and moved pygame.quit to the beginning

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        pygame.Surface.fill(0, 0, 0)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")

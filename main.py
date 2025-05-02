6a204b13ee4039c0d46c7e58d72c8ada29371a80
High-Quality-Dog-Water <brettd.work@gmail.com>
2025-05-01 18:59:05 -0600

    C: finished player movement

--- main.py
+++ main.py
@@ -3,16 +3,25 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        screen.fill((0, 0, 0))
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
